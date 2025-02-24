206b3b3787fa6606478e84821d52548098257a6b
isahmet089 <isahmet089@gmail.com>
2025-02-24 03:05:39 +0300

    Implement authentication logic for login and registration in authController

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,10 +1,34 @@
+const User=require('../models/User');
 
 const loginContoller=async(req,res)=>{
-  // login logic
+    try {
+      let {email,password}=req.body;
+      const user = await User.findOne({email});
+      console.log(user);
+      if(!user){
+        return res.status(400).json({message:"Kullanıcı bulunamadı"});
+      }
+      if(password !== user.password){
+        return res.status(400).json({message:"Hatalı şifre"});
+      }
+      res.status(200).json({message:"Giriş başarılı"});
+    } catch (error) {
+      res.status(500).json({message:error.message});
+    }
 };
 
 const registerController=async(req,res)=>{
-  // register logic
+  try {
+    let user = new User(req.body);
+    const emailCheck = await User.findOne({email:user.email});
+    if(emailCheck){
+      return res.status(400).json({message:"Email already exists"});
+    }
+  await user.save();
+  res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({message:error.message});
+  }
 };
 
 module.exports={loginContoller,registerController};
